f852bd66db1acfe99c065783c6205729ec47476f
xuan <s1114670@pu.edu.tw>
2024-05-22 15:43:15 +0800

    v3_黃玟瑄

--- app/src/main/java/tw/edu/pu/s1114670/s11146708/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1114670/s11146708/MainActivity.kt
@@ -3,86 +3,72 @@ package tw.edu.pu.s1114670.s11146708
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import tw.edu.pu.s1114670.s11146708.ui.theme.S11146708Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            S11146708Theme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainScreen()
-                }
-            }
+            MyApp()
         }
     }
 }
 
 @Composable
-fun MainScreen() {
-    var selectedScreen by remember { mutableStateOf("簡介") }
+fun MyApp() {
+    var isAuthorView by remember { mutableStateOf(false) }
+    var imageRes by remember { mutableIntStateOf(R.drawable.service) }
+    val alpha: Float by animateFloatAsState(
+        targetValue = if (isAuthorView) 1f else 0f,
+        animationSpec = tween(durationMillis = 3000), label = ""
+    )
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        TopImage()
-
-        Row(
-            modifier = Modifier.padding(16.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Button(onClick = { selectedScreen = "簡介" }) {
-                Text("顯示簡介喔")
-            }
-            Button(onClick = { selectedScreen = "主要機構" }) {
-                Text("顯示主要機構喔")
-            }
-        }
-
-        SecondScreen(selectedScreen)
-    }
-}
-
-@Composable
-fun TopImage() {
-    Image(
-        painter = painterResource(id = R.drawable.maria),
-        contentDescription = null,
         modifier = Modifier
-            .width(150.dp)
-            .height(100.dp),
-        contentScale = ContentScale.Fit
-    )
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(text = if (isAuthorView) "關於App作者" else "瑪利亞基金會服務總覽")
+
+        val image: Painter = painterResource(id = if (isAuthorView) R.drawable.author else imageRes)
+
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.alpha(alpha)
+        )
+
+        Button(onClick = {
+            isAuthorView = !isAuthorView
+            imageRes = if (isAuthorView) {
+                R.drawable.author
+            } else {
+                R.drawable.service
+            }
+        }) {
+            Text(text = if (isAuthorView) "服務總覽" else "作者：資管系黃玟瑄")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    S11146708Theme {
-        MainScreen()
-    }
+fun S11146708Preview() {
+    MyApp()
 }
